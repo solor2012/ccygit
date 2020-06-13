e70aa2a838a7f4068d6c84badbc31377a8e3d44f
solor <664149340@qq.com>
2020-06-13 09:45:04 +0800

    shop cart demo

---
+++ webapp/initMockServer.js
@@ -0,0 +1,9 @@
+sap.ui.define([
+    "sap/ui/demo/cart/localService/mockserver"
+], function(mockserver) {
+    'use strict';
+    
+    mockserver.init();
+
+    sap.ui.require(["sap/ui/core/ComponentSupport"]);
+});
